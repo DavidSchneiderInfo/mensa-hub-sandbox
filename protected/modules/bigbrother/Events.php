2b036afa54eba785ca8ce1ae3425dc364a443af0
David <kontakt@davidschneider.info>
2024-08-31 09:56:57 +0200

    Adding report link for posts

--- protected/modules/bigbrother/Events.php
+++ protected/modules/bigbrother/Events.php
@@ -2,13 +2,17 @@
 
 namespace  BigBrother;
 
+use BigBrother\helpers\Permission;
+use BigBrother\widgets\ReportContentLink;
+use humhub\modules\comment\widgets\CommentControls;
+use humhub\modules\ui\menu\MenuLink;
 use Yii;
 use yii\helpers\Url;
 
 class Events
 {
     /**
-     * Defines what to do if admin menu is initialized.
+     * The menu entry in the Administration menu. This will open the module config page.
      *
      * @param $event
      */
@@ -21,6 +25,43 @@ class Events
             'icon' => '<i class="fa fa-adjust"></i>',
             'isActive' => (Yii::$app->controller->module && Yii::$app->controller->module->id == 'BigBrother' && Yii::$app->controller->id == 'admin'),
             'sortOrder' => 99999,
+        ]);
+    }
+
+    /**
+     * Menu entry for a comment
+     *
+     * @param $event
+     * @return void
+     */
+    public static function onCommentControlsInit($event)
+    {
+        /** @var CommentControls $menu */
+        $menu = $event->sender;
+
+        if (!Permission::canReportComment($menu->comment)) {
+            return;
+        }
+
+        $menu->addEntry(new MenuLink([
+            'label' => 'Report',
+            'icon' => 'fa-exclamation-triangle',
+            'url' => '#',
+            'htmlOptions' => [
+                'data-action-click' => 'ui.modal.load',
+                'data-action-click-url' => Url::to([
+                    '/BigBrother/report', 'contentId' => $menu->comment->content->id,
+                    'commentId' => $menu->comment->id
+                ])
+            ],
+            'sortOrder' => 1000,
+        ]));
+    }
+
+    public static function onWallEntryControlsInit($event)
+    {
+        $event->sender->addWidget(ReportContentLink::class, [
+            'record' => $event->sender->object
         ]);
     }
 }
--- protected/modules/bigbrother/config.php
+++ protected/modules/bigbrother/config.php
@@ -2,17 +2,17 @@
 
 use BigBrother\Events;
 use humhub\modules\admin\widgets\AdminMenu;
-use humhub\widgets\TopMenu;
+use humhub\modules\comment\widgets\CommentControls;
+use humhub\modules\ui\menu\widgets\Menu;
+use humhub\modules\content\widgets\WallEntryControls;
 
 return [
 	'id' => 'BigBrother',
 	'class' => 'BigBrother\Module',
 	'namespace' => 'BigBrother',
 	'events' => [
-		[
-			'class' => AdminMenu::class,
-			'event' => AdminMenu::EVENT_INIT,
-			'callback' => [Events::class, 'onAdminMenuInit']
-		],
+		[AdminMenu::class, AdminMenu::EVENT_INIT, [Events::class, 'onAdminMenuInit']],
+        [CommentControls::class, Menu::EVENT_INIT, [Events::class, 'onCommentControlsInit']],
+        [WallEntryControls::class, WallEntryControls::EVENT_INIT, [Events::class, 'onWallEntryControlsInit']],
 	],
 ];
